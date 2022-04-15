a4303a131c4a35a3137f6e498f133f0a62e7e523
root <root@ip-172-31-2-245.ap-south-1.compute.internal>
2022-04-15 12:45:30 +0000

    c

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,11 +40,7 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-    @Test
-  public void greetShouldIncludeGreetingMessage1() {
-    String someone = "World , Thanks for having me";
-   
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())))>
-  }
+
+  
 }
 
